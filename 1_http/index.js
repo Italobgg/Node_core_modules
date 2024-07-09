fea48df522e17f484d478821280fc7dabd507260
ItaloBGG <italobggdev@gmail.con>
2024-07-09 11:28:31 -0300

    URL

--- 1_http/index.js
+++ 1_http/index.js
@@ -1,12 +1,12 @@
-const http = require('http')
+const http = require("http");
 
-const port = 3000
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.write('oi http')
-    res.end()
-})
+  res.write("oi http");
+  res.end();
+});
 
 server.listen(port, () => {
-    console.log(`Servidor on na porta ${port}`)
-})
+  console.log(`Servidor on na porta ${port}`);
+});
